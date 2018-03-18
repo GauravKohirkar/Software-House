17e73c6bb491fdaaea65ada0024df734c351acdb
GauravKohirkar <gauravkohirkar@gmail.com>
2018-03-19 07:54:27 +1100

    Adding to master

---
+++ Software-House/Data/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Software_House.Data.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
