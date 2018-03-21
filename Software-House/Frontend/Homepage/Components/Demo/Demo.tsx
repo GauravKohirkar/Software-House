ab405acbc31bc9c45c67e0aebc6b91fd78fb8c4b
GauravKohirkar <gauravkohirkar@gmail.com>
2018-03-21 23:59:40 +1100

    react stuff from gaurav

--- Software-House/Frontend/Homepage/Components/Demo/Demo.tsx
+++ Software-House/Frontend/Homepage/Components/Demo/Demo.tsx
@@ -6,7 +6,7 @@ class Demo extends React.Component<any, any> {
     public render() {
         return (
             <div className="Demo">
-                <p>das.NET + React, from PoznajProgramowanie.pasdl</p>
+                <p>reactstuff from gaurav</p>
             </div>
         )
     }
